73a6d96028bacaed625b40a3429158dfa4b5e0a2
Yamila Moreno <yamila.ms@gmail.com>
2016-04-03 21:06:47 +0200

    IT WORKS! YAY!!! \o/

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp"),
     jade = require("gulp-jade"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    concat = require("gulp-concat"),
+    connect = require('gulp-connect');
 
 var paths = {};
 paths.base = "";
@@ -10,47 +12,41 @@ paths.jade = [
     paths.base + "**/*.jade"
 ];
 
-
-//paths.htmlPartials = [
-//    paths.tmp + "partials/**/*.html",
-//    paths.tmp + "modules/**/*.html",
-//    "!" + paths.tmp + "partials/includes/**/*.html",
-//    "!" + paths.tmp + "/modules/**/includes/**/*.html"
-//];
-
-
 paths.sass = [
     paths.base + "sass/*.scss",
 ];
 
+gulp.task('dev-webserver', function() {
+  connect.server({
+    root: paths.dist,
+    livereload: true,
+    fallback: paths.dist + 'index.html'
+  });
+});
+
+gulp.task('webserver-reload', function(){
+    connect.reload()
+});
+
+
 gulp.task("styles", function() {
   return gulp.src(paths.sass)
-    .pipe($.plumber())
-    .pipe($.cached())
-    .pipe($.sass({
+    .pipe(sass({
       endless:"true",
       errLogToConsole: true,
       includePaths: [
         paths.sass,
       ]
     }))
-    .pipe($.autoprefixer({
-      browsers: ["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1"],
-      cascade: false
-    }))
-    .pipe($.remember("styles"))
-    .pipe($.concat("style.css"))
+    .pipe(concat("style.css"))
     .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task("templates", function() {
     return gulp.src(paths.jade)
-        .pipe(plumber())
-        .pipe(cached("jade"))
-        .pipe(jade({pretty: true, locals:{v:version}}))
+        .pipe(jade({pretty: true}))
         .pipe(gulp.dest(paths.dist));
 });
-
 
 gulp.task("compile", ["styles", "templates"]);
 
